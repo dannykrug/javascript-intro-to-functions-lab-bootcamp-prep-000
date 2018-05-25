07c63e8e086581da33bb151bed6e540efc403a90
Daniel Krug <dannykrug@gmail.com>
2018-05-25 06:50:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,10 +12,11 @@ function logWhisper(string) {
 }
 function sayHiToGrandma(string) {
   if (string.toLowerCase() === string)
-}
   return ("I can't hear you!")
-  if (string.toUpperCase() === string)
+}
+  if (string.toUpperCase() === string) {
   return ("YES INDEED!")
-  if ("I love you, Grandma." === string)
-  return ("I love you, too")
+  }
+  if (string === "I love you, Grandma."){
+  return ("I love you, too.")
 }
